bb819620f4d96b45b0ce55f2acf3d96755d4c95d
Taioh Kubota <kubotataioh@gmail.com>
2022-07-12 21:50:33 -0400

    adding axios custom hook

--- src/PlayingCardList.js
+++ src/PlayingCardList.js
@@ -3,22 +3,18 @@ import uuid from "uuid";
 import axios from "axios";
 import PlayingCard from "./PlayingCard";
 import "./PlayingCardList.css";
-
+import {useAxios} from "./hooks"
 /* Renders a list of playing cards.
  * Can also add a new card at random. */
 function CardTable() {
-  const [cards, setCards] = useState([]);
-  const addCard = async () => {
-    const response = await axios.get(
-      "https://deckofcardsapi.com/api/deck/new/draw/"
-    );
-    setCards(cards => [...cards, { ...response.data, id: uuid() }]);
-  };
+  const[cards, addCard, clearCards] = useAxios("cards","https://deckofcardsapi.com/api/deck/new/draw/");
+
   return (
     <div className="PlayingCardList">
       <h3>Pick a card, any card!</h3>
       <div>
         <button onClick={addCard}>Add a playing card!</button>
+        <button onClick={clearCards}>Clear table</button>
       </div>
       <div className="PlayingCardList-card-area">
         {cards.map(cardData => (
--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,6 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from "axios"
+import useLocalStorage from '../../react-cards-pokemon-solution/src/hooks';
 
 const useFlip = () =>{
     const[state,setState] = useState(true);
@@ -9,4 +11,31 @@ const useFlip = () =>{
 
     return [state,flipCard]
 }
-export default useFlip;
+
+const useAxios = (key,url) =>{
+    const [response,setResponse] = useLocalStorage(key);
+
+    const addResponseData = async (formatter = data => data,restOfUrl = "") =>{
+    const response = await axios.get(`${baseUrl}${restOfUrl}`);
+    setResponse(data => [...data, formatter(response.data)]);
+    };
+
+    const clearResponses = () => setResponse([]);
+
+    return [response, addResponseData, clearResponses];
+}
+
+const useLocalStorageState = (key, defaultValue) =>{
+ const [state,setState] = useState(() =>{
+     let value = JSON.parse(window.localStorage.getItem(key) || defaultValue);
+        return value;
+ });
+ useEffect(() =>{
+     window.localStorage.setItem(key,state)
+ },[key,state])
+
+ return [state,setState]
+}
+export default useLocalStorage;
+
+export { useFlip, useAxios, useLocalStorage};
